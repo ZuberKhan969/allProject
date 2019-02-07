ce928883cfd8efe49df46c09c25de755584d3321
zuberpathan969 <zuber.pathan969@gmail.com>
2019-02-07 15:04:46 +0530

    Encapsulation

---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            deparment dt = new deparment();
+            dt.setDepartment("Zuber");
+            Console.WriteLine("This is set :-" + dt.setDepartment());
+            Console.Read();
+        }
+    }
+    public class deparment
+    {
+        private string DeparmentName;
+            
+        public string getDepartment()
+        {
+            return DeparmentName;
+        }
+        public void setDepartment(string a)
+        {
+            DeparmentName = a;
+        }
+    }
+}
