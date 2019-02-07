44ad876a79a08b0af2c10059fce1134313bf376c
zuberpathan969 <zuber.pathan969@gmail.com>
2019-02-07 14:47:31 +0530

    CodeFirst

---
+++ CodeFirst/CodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        // GET: Home
+        public ActionResult Index()
+        {
+            DataContext db = new DataContext();
+            var data = db.Employees.Count();
+
+            return View(data);
+        }
+    }
+}
---
+++ CodeFirst/CodeFirst/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int empid { get; set; }
+        public string empName { get; set; }
+        public string empAddress { get; set; }
+
+
+    }
+}
